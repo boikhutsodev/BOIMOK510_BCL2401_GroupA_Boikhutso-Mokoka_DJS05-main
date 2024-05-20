8dd8e631969ee19819bf756d5dcb163b06dbecc1
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-05-20 16:34:42 +0200

    creating a store

---
+++ Store.js
@@ -0,0 +1,22 @@
+export function createStore(reducer) {
+    let state = reducer(undefined, {});
+    const listeners = [];
+
+    return {
+        dispatch(action) {
+            state = reducer(state, action);
+            listeners.forEach(listener => listener{});
+        },
+        getState() {
+            return state;
+        },
+        subscribe(listener) {
+            listeners.push(listener);
+            return() => {
+                const index = listeners.indexOf(listener);
+                listeners.splice(index, 1);
+            };
+
+        },
+    };
+}
---
+++ reducer.js
@@ -0,0 +1,4 @@
+import { action } from "./action.js";
+import { initialState } from "./initialState.js";
+
+export function reducer(state)
