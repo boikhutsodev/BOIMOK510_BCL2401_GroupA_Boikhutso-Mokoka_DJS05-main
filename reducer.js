54b16b8a9965f436eea654688e2289a86f589122
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-05-20 16:44:54 +0200

    Reducer function

--- reducer.js
+++ reducer.js
@@ -1,4 +1,15 @@
 import { action } from "./action.js";
 import { initialState } from "./initialState.js";
 
-export function reducer(state)
+export function reducer(state = initialState, actions) {
+  switch (actions.type) {
+    case action.INCREMENT:
+      return { count: state.count + 1 };
+    case action.DECREMENT:
+      return { count: state.count - 1 };
+    case action.RESET:
+      return { count: 0 };
+    default:
+      return state;
+  }
+}
